caa245fd55cdecfd0c51c86843ac502b64f26716
hong li <lihongitchina@gmail.com>
2020-06-03 23:24:07 +0800

    optimized DUPLICATED LINES

--- convert/format.go
+++ convert/format.go
@@ -4,49 +4,20 @@ import (
 	"fmt"
 )
 
+// units
+var units = []string{"B", "KB", "MB", "GB", "TB", "PB"}
+
 // ReadableSize convert byte to KB,MB,GB,TB,PB
 func ReadableSize(raw float64) string {
 	var t float64 = 1024
 	var d float64 = 1
 
-	if raw < t {
-		return fmt.Sprintf("%.1fB", raw/d)
+	for _, unit := range units {
+		if raw < t {
+			return fmt.Sprintf("%.1f%s", raw/d, unit)
+		}
+		d *= 1024
+		t *= 1024
 	}
-
-	d *= 1024
-	t *= 1024
-
-	if raw < t {
-		return fmt.Sprintf("%.1fKB", raw/d)
-	}
-
-	d *= 1024
-	t *= 1024
-
-	if raw < t {
-		return fmt.Sprintf("%.1fMB", raw/d)
-	}
-
-	d *= 1024
-	t *= 1024
-
-	if raw < t {
-		return fmt.Sprintf("%.1fGB", raw/d)
-	}
-
-	d *= 1024
-	t *= 1024
-
-	if raw < t {
-		return fmt.Sprintf("%.1fTB", raw/d)
-	}
-
-	d *= 1024
-	t *= 1024
-
-	if raw < t {
-		return fmt.Sprintf("%.1fPB", raw/d)
-	}
-
 	return "TooLarge"
 }
