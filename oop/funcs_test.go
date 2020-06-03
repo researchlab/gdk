a506610fbf91ce30c365c022798c92aa9fa23672
hong li <lihongitchina@gmail.com>
2020-06-03 22:58:54 +0800

    [update] funcs unit tests

--- oop/funcs_test.go
+++ oop/funcs_test.go
@@ -2,52 +2,68 @@ package oop
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestNewFuncs01(t *testing.T) {
-	nfuncs := NewFuncs()
+func TestNewFuncs(t *testing.T) {
+	want := make(Funcs, 0)
+	if f := NewFuncs(); !reflect.DeepEqual(f, want) {
+		t.Fatalf("NewFuncs() = %v, want:%v", f, want)
+	}
+}
+
+func TestFuncs_Bind(t *testing.T) {
+	type fields struct {
+		name string
+		fn   interface{}
+	}
 	tests := []struct {
-		Name    string
-		F       interface{}
-		inputs  []interface{}
-		outputs []interface{}
+		name    string
+		fields  fields
+		wantErr bool
 	}{
-		{"Add", Add, []interface{}{1, 2}, []interface{}{3}},
-		{"sum", sum, []interface{}{1, 2, 3}, []interface{}{6, true}},
+		{name: "bind success", fields: fields{name: "Add", fn: func(a, b int) int { return a + b }}, wantErr: false},
+		{name: "panics if the fn type's Kind is not Func", fields: fields{name: "nil", fn: nil}, wantErr: true},
 	}
 	for _, test := range tests {
-		if err := nfuncs.Bind(test.Name, test.F); err != nil {
-			t.Fatalf("Bind invalid, test:%v, err:%v", test, err)
-		}
-		if outputs, err := nfuncs.Call(test.Name, test.inputs...); err != nil || len(outputs) != len(test.outputs) {
-			t.Fatalf("Call invalid, test:%v, err:%v", test, err)
-		} else {
-			for i, v := range outputs {
-				if fmt.Sprintf("%v", v) != fmt.Sprintf("%v", test.outputs[i]) {
-					t.Fatalf("Call invalid, out:%v, expected:%v", v, test.outputs[i])
-				}
+		t.Run(test.name, func(t *testing.T) {
+			f := NewFuncs()
+			if err := f.Bind(test.fields.name, test.fields.fn); (err != nil) != test.wantErr {
+				t.Errorf("f.Bind() err =%v, wantErr:%v", err, test.wantErr)
 			}
-		}
-	}
-	// negative cases
-	if err := nfuncs.Bind("Add", nil); err == nil {
-		t.Fatal("Bind invalid")
-	}
-	if outputs, err := nfuncs.Call("unknown", []interface{}{}); err == nil {
-		t.Fatalf("Call invalid, outputs:%v", outputs)
-	}
-	if outputs, err := nfuncs.Call("Add", []interface{}{}); err == nil {
-		t.Fatalf("Call invalid, outputs:%v", outputs)
+		})
 	}
 }
 
-func Add(a, b int) int {
-	return a + b
-}
-
-func sum(c, d, e int) (int, bool) {
-	return c + d + e, true
+func TestFuncs_Call(t *testing.T) {
+	type fields struct {
+		name   string
+		params []interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantR   []reflect.Value
+		wantErr bool
+	}{
+		{name: "call success", fields: fields{name: "Add", params: []interface{}{1, 2}}, wantR: []reflect.Value{reflect.ValueOf(3)}, wantErr: false},
+		{name: "function name doesn't exist", fields: fields{name: "Sum"}, wantErr: true},
+		{name: "not enough params", fields: fields{name: "Add", params: []interface{}{1}}, wantErr: true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f := NewFuncs()
+			f.Bind("Add", func(a, b int) int { return a + b })
+			gotR, err := f.Call(test.fields.name, test.fields.params...)
+			if (err != nil) != test.wantErr {
+				t.Errorf("f.Call() err=%v, wantErr:%v", err, test.wantErr)
+			}
+			if fmt.Sprintf("%v", gotR) != fmt.Sprintf("%v", test.wantR) {
+				t.Errorf("f.Call() gotR:%v, wantR:%v", gotR, test.wantR)
+			}
+		})
+	}
 }
 
 type Person struct {
@@ -63,7 +79,7 @@ func (p Person) ShowAge() int {
 	return p.Age
 }
 
-func TestNewFuncs02(t *testing.T) {
+func TestFuncs_Call02(t *testing.T) {
 	persons := []Person{
 		{"mike", 10},
 		{"mike", 20},
