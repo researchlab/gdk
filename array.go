fa63e69b24099abc43b35b23073d07bad88fb459
lihong <lihongitchina@gmail.com>
2023-04-18 14:53:51 +0800

    array tool kits

---
+++ array.go
@@ -0,0 +1,44 @@
+package gdk
+
+// Contains return true if this array contains the specified element.
+func Contains[E string | int64 | int | float64](data []E, key E) bool {
+	if len(data) == 0 {
+		return false
+	}
+	for i := 0; i < len(data); i++ {
+		if data[i] == key {
+			return true
+		}
+	}
+	return false
+}
+
+// ArrayMerge  merge two or more arrays into a new array
+func ArrayMerge[E string | int64 | int](a, b []E, c ...[]E) (data []E) {
+	data = append(a, b...)
+	for _, v := range c {
+		data = append(data, v...)
+	}
+	return data
+}
+
+// ArraySum  sum of the given array
+func ArraySum[E int64 | int | float64](data []E) (sum E) {
+	for i := 0; i < len(data); i++ {
+		sum += data[i]
+	}
+	return sum
+}
+
+// ArrayUnique remove Duplicate item of the given array
+func ArrayUnique[E int64 | int | string](in []E) (out []E) {
+	m := make(map[E]struct{})
+	out = []E{}
+	for i := 0; i < len(in); i++ {
+		if _, ok := m[in[i]]; !ok {
+			out = append(out, in[i])
+			m[in[i]] = struct{}{}
+		}
+	}
+	return out
+}
---
+++ array_test.go
@@ -0,0 +1,151 @@
+package gdk
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	R := func(t *testing.T, got, want bool) {
+		if got != want {
+			t.Errorf("Contains()=%v, want %v", got, want)
+		}
+	}
+	t.Run("String Contains true", func(t *testing.T) {
+		R(t,
+			Contains([]string{"mike", "jack"}, "mike"), // got
+			true, // want
+		)
+	})
+	t.Run("String Contains false", func(t *testing.T) { R(t, Contains([]string{"mike", "jack"}, "hello"), false) })
+	t.Run("int Contains true", func(t *testing.T) {
+		R(t,
+			Contains([]int{1, 2}, 1), // got
+			true,                     // want
+		)
+	})
+	t.Run("int Contains false", func(t *testing.T) {
+		R(t,
+			Contains([]int{1, 2}, 3), // got
+			false,                    // want
+		)
+	})
+	t.Run("float64 Contains true", func(t *testing.T) {
+		R(t,
+			Contains([]float64{1.1, 2.0}, 1.1), // got
+			true,                               // want
+		)
+	})
+	t.Run("int Contains false", func(t *testing.T) {
+		R(t,
+			Contains[float64]([]float64{1, 2.2}, 3.0), // got
+			false, // want
+		)
+	})
+
+}
+
+func TestArrayMerge(t *testing.T) {
+	t.Run("nil merge array", func(t *testing.T) {
+		if got := ArrayMerge(nil, []int{1, 2}); !reflect.DeepEqual(got, []int{1, 2}) {
+			t.Errorf("ArrayMerge(nil, int{1,2})=%+v, want %v", got, "[]int{1, 2}")
+		}
+	})
+	t.Run("array merge nil", func(t *testing.T) {
+		if got := ArrayMerge([]int{1, 2}, nil); !reflect.DeepEqual(got, []int{1, 2}) {
+			t.Errorf("ArrayMerge(int{1,2},nil)=%+v, want %v", got, "[]int{1, 2}")
+		}
+	})
+	t.Run("large merge small", func(t *testing.T) {
+		if got := ArrayMerge([]int{1, 2, 3}, []int{1, 5}); !reflect.DeepEqual(got, []int{1, 2, 3, 1, 5}) {
+			t.Errorf("got %v, want %v", got, "[]int{1,2,3,1,5}")
+		}
+	})
+	t.Run("small merge large", func(t *testing.T) {
+		if got := ArrayMerge([]int{1, 2}, []int{1, 5, 6, 7}); !reflect.DeepEqual(got, []int{1, 2, 1, 5, 6, 7}) {
+			t.Errorf("got %v, want %v", got, "[]int{1,2,1,5,6,7}")
+		}
+	})
+	t.Run("more than 2 array merged", func(t *testing.T) {
+		if got := ArrayMerge([]int{1, 2}, []int{1}, []int{1, 5, 7}, []int{9, 0}, []int{1, 1}); !reflect.DeepEqual(got, []int{1, 2, 1, 1, 5, 7, 9, 0, 1, 1}) {
+			t.Errorf("got %v, want %v", got, "[]int{1,2,1,1,5,7,9,0,1,1}")
+		}
+	})
+	t.Run("merge effect", func(t *testing.T) {
+		a := []string{"a", "b", "cc"}
+		b := []string{"d", "e"}
+		c := []string{}
+		if got := ArrayMerge(a, b, c, nil); !reflect.DeepEqual(got, []string{"a", "b", "cc", "d", "e"}) {
+			t.Errorf("got %v, want %v", got, "[]string{a,b,cc,d,e}")
+		} else {
+			got[0] = "111"
+			got[3] = "222"
+			if !reflect.DeepEqual(a, []string{"a", "b", "cc"}) {
+				t.Errorf("got %v, want %v", a, "[]string{a,b,cc}")
+			}
+			if !reflect.DeepEqual(b, []string{"d", "e"}) {
+				t.Errorf("got %v, want %v", b, "[]string{d,e}")
+			}
+			if !reflect.DeepEqual(got, []string{"111", "b", "cc", "222", "e"}) {
+				t.Errorf("got %v, want %v", got, "[]string{111,b,cc,222,e}")
+			}
+		}
+
+	})
+}
+
+func TestArraySum(t *testing.T) {
+	t.Run("sum of array int", func(t *testing.T) {
+		if got := ArraySum([]int{1, 2, 3}); got != 6 {
+			t.Errorf("got %v want 6", got)
+		}
+	})
+
+	t.Run("sum of array int64", func(t *testing.T) {
+		if got := ArraySum([]int64{1, 2, 3}); got != 6 {
+			t.Errorf("got %v want 6", got)
+		}
+	})
+	t.Run("sum of array float64", func(t *testing.T) {
+		if got := ArraySum([]float64{1.0, 2.1, 3.5}); got != 6.6 {
+			t.Errorf("got %v want 6.6", got)
+		}
+	})
+
+	t.Run("sum of array nil", func(t *testing.T) {
+		if got := ArraySum[int](nil); got != 0 {
+			t.Errorf("got %v want 0", got)
+		}
+	})
+	t.Run("sum of empty array", func(t *testing.T) {
+		if got := ArraySum([]int{}); got != 0 {
+			t.Errorf("got %v want 0", got)
+		}
+	})
+}
+
+func TestArrayUnique(t *testing.T) {
+	t.Run("remove duplicate item of array int64", func(t *testing.T) {
+		if got := ArrayUnique([]int64{1, 1, 1, 2, 2}); !reflect.DeepEqual(got, []int64{1, 2}) {
+			t.Errorf("got %v, want []int64[1,2]", got)
+		}
+	})
+
+	t.Run("remove duplicate item of array nil", func(t *testing.T) {
+		if got := ArrayUnique([]int64{}); !reflect.DeepEqual(got, []int64{}) {
+			t.Errorf("got %v, want []int64{}", got)
+		}
+	})
+
+	t.Run("remove duplicate item of array string", func(t *testing.T) {
+		if got := ArrayUnique([]string{"a", "a", "aa", "ab"}); !reflect.DeepEqual(got, []string{"a", "aa", "ab"}) {
+			t.Errorf("got %v, want []string{a,aa,ab}", got)
+		}
+	})
+
+	t.Run("remove duplicate item of array int", func(t *testing.T) {
+		if got := ArrayUnique([]int{1, 2, 3}); !reflect.DeepEqual(got, []int{1, 2, 3}) {
+			t.Errorf("got %v, want []int{1,2,3}", got)
+		}
+	})
+}
